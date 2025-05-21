3d1610055fe6473cf774d17e48ef40ff082e06e0
Cody Williams <cody@codywilliams.dev>
2025-05-20 19:11:29 -0700

    updated schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_26_005530) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_21_020437) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "account_active_session_keys", primary_key: ["account_id", "session_id"], force: :cascade do |t|
+    t.bigint "account_id", null: false
+    t.string "session_id", null: false
+    t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "last_use", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.index ["account_id"], name: "index_account_active_session_keys_on_account_id"
+  end
 
   create_table "account_login_change_keys", force: :cascade do |t|
     t.string "key", null: false
@@ -37,10 +45,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_26_005530) do
     t.integer "status", default: 1, null: false
     t.citext "email", null: false
     t.string "password_hash"
+    t.string "name"
+    t.string "username"
+    t.string "phone"
     t.index ["email"], name: "index_accounts_on_email", unique: true, where: "(status = ANY (ARRAY[1, 2]))"
     t.check_constraint "email ~ '^[^,;@ \r\n]+@[^,@; \r\n]+.[^,@; \r\n]+$'::citext", name: "valid_email"
   end
 
+  add_foreign_key "account_active_session_keys", "accounts"
   add_foreign_key "account_login_change_keys", "accounts", column: "id"
   add_foreign_key "account_password_reset_keys", "accounts", column: "id"
   add_foreign_key "account_verification_keys", "accounts", column: "id"
